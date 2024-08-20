1e1c42b24dd03cc7ef444d309930cf95554de0da
jstevenson02 <jacobryanstevenson@gmail.com>
2024-08-20 09:14:41 -0400

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+import { useAuth } from "@clerk/nextjs";
+import { useUser } from "@clerk/nextjs";
+
 export default function Home() {
+  const { isLoaded, isSignedIn, user } = useUser();
+
+  if (!isLoaded || !isSignedIn) {
+    return null;
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello World, Welcome to Family Ties
+      Hello {user.firstName} {user.lastName}, Welcome to Family Ties Your email
+      is {user.emailAddresses[0].emailAddress}
     </main>
   );
 }
